fe3b5f82e3aff863079600808a1910f821775d7d
Samuel Lima <samz.xit@gmail.com>
2026-08-12 13:14:03 -0300

    creating project structure

---
+++ src/main/java/com/desafio/globo/GloboApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.globo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/globo/GloboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.globo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
