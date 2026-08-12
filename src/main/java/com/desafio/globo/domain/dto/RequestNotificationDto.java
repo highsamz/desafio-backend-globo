1cb1ab76ffcb094d39d5f008910f2839214ffd50
Samuel Lima <samz.xit@gmail.com>
2026-08-12 13:55:49 -0300

    Creating DTOs with core fields

--- src/main/java/com/desafio/globo/domain/dto/RequestNotificationDto.java
+++ src/main/java/com/desafio/globo/domain/dto/RequestNotificationDto.java
@@ -1,4 +1,8 @@
 package com.desafio.globo.domain.dto;
 
-public class RequestNotificationDto {
-}
+import com.desafio.globo.domain.enums.NotificationType;
+
+public record RequestNotificationDto (
+        String subscriptionId,
+        NotificationType type
+) {}
