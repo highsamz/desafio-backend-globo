9dfb799000f688d45e3de9080b6a05f3d80e3ed6
Samuel Lima <samz.xit@gmail.com>
2026-08-13 01:42:35 -0300

    renaming dto

--- src/main/java/com/desafio/globo/domain/dto/RequestNotificationDto.java
+++
@@ -1,8 +0,0 @@
-package com.desafio.globo.domain.dto;
-
-import com.desafio.globo.domain.enums.NotificationType;
-
-public record RequestNotificationDto (
-        String subscriptionId,
-        NotificationType type
-) {}
