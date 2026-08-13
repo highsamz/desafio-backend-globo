0eea8d6aa18df25de23e4097868e5e1567b1e371
Samuel Lima <samz.xit@gmail.com>
2026-08-13 01:46:24 -0300

    Defining the Publisher

---
+++ src/main/java/com/desafio/globo/infra/messaging/publisher/RabbitSubscriptionPublisher.java
@@ -0,0 +1,23 @@
+package com.desafio.globo.infra.messaging.publisher;
+
+import com.desafio.globo.application.MessagePublisher;
+import com.desafio.globo.domain.dto.RequestSubscriptionDto;
+import com.desafio.globo.infra.messaging.properties.SubscriptionQueueProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class RabbitSubscriptionPublisher implements MessagePublisher {
+    private final SubscriptionQueueProperties subscriptionQueueProperties;
+    private final RabbitTemplate rabbitTemplate;
+
+    @Override
+    public void publish(RequestSubscriptionDto requestSubscriptionDto) {
+        log.info("Notifying queue: {} of text{}", subscriptionQueueProperties.getName(), requestSubscriptionDto);
+        rabbitTemplate.convertAndSend(subscriptionQueueProperties.getName(),requestSubscriptionDto);
+    }
+}
