04f547fa3b9169d85d23e18b6394c408df2494d5
Samuel Lima <samz.xit@gmail.com>
2026-08-14 15:22:41 -0300

    Since I'm using MongoDB, which isn't a relational database, it doesn't make sense to maintain that repository

--- src/main/java/com/desafio/globo/infra/persistence/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.desafio.globo.infra.persistence;
-
-import com.desafio.globo.domain.entity.Status;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StatusRepository extends MongoRepository<Status, Integer> {
-}
