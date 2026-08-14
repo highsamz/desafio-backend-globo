c55adc4d64ba3ae5811772434abf497a44f35eb9
Samuel Lima <samz.xit@gmail.com>
2026-08-14 15:29:06 -0300

    Creating a persistence constructor for the eventHistory object and changing the id to a string because I'm using MongoDB

--- src/main/java/com/desafio/globo/domain/entity/EventHistory.java
+++ src/main/java/com/desafio/globo/domain/entity/EventHistory.java
@@ -1,5 +1,6 @@
 package com.desafio.globo.domain.entity;
 
+import com.desafio.globo.domain.enums.NotificationType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,14 +10,17 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document(collection = "event_history")
-@Getter
-@Setter
-@AllArgsConstructor
 public class EventHistory {
 
     @Id
-    private Integer id;
-    private String type;
+    private String id;
+    private NotificationType type;
     private String subscriptionId;
     private LocalDateTime createdAt;
+
+    public EventHistory(NotificationType type, String subscriptionId){
+        this.type = type;
+        this.subscriptionId = subscriptionId;
+        this.createdAt = LocalDateTime.now();
+    }
 }
