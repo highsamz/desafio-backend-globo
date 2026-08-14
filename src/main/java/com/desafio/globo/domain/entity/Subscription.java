78ee90159ca97b43837282a656c2706ea1fbab7d
Samuel Lima <samz.xit@gmail.com>
2026-08-14 15:27:39 -0300

    mproving the subscription entity and the way the methods create, cancel, and activate subscriptions work

--- src/main/java/com/desafio/globo/domain/entity/Subscription.java
+++ src/main/java/com/desafio/globo/domain/entity/Subscription.java
@@ -1,11 +1,16 @@
 package com.desafio.globo.domain.entity;
 
+import com.desafio.globo.api.request.RequestSubscriptionDto;
 import com.desafio.globo.domain.enums.NotificationType;
+import com.desafio.globo.domain.enums.SubscriptionStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
 
 @Getter
 @Setter
@@ -14,7 +19,26 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Subscription {
 
     @Id
-    private Long id;
-    private String subscription;
-    private NotificationType notificationType;
+    private String subscriptionId;
+    private SubscriptionStatus statusId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Subscription(String subscriptionId) {
+        this.subscriptionId = subscriptionId;
+        this.statusId = SubscriptionStatus.ACTIVE;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void activate() {
+        this.statusId = SubscriptionStatus.ACTIVE;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public void cancel() {
+        this.statusId = SubscriptionStatus.CANCELED;
+        this.updatedAt = LocalDateTime.now();
+    }
+
 }
