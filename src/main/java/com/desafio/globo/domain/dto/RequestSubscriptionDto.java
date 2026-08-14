2304a406fa99846a64214fd2277f77711da9d963
Samuel Lima <samz.xit@gmail.com>
2026-08-14 00:58:54 -0300

    Changing the DTO to the api layer.

--- src/main/java/com/desafio/globo/domain/dto/RequestSubscriptionDto.java
+++
@@ -1,8 +0,0 @@
-package com.desafio.globo.domain.dto;
-
-import com.desafio.globo.domain.enums.NotificationType;
-
-public record RequestSubscriptionDto(
-        String subscription,
-        NotificationType notificationType
-) {}
