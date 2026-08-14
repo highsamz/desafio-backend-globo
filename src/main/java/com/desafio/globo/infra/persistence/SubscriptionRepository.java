5d00e86dd6b0b84f9548c43bd6615de145859f7a
Samuel Lima <samz.xit@gmail.com>
2026-08-14 15:30:09 -0300

    Creating a search by subscriptionId

--- src/main/java/com/desafio/globo/infra/persistence/SubscriptionRepository.java
+++ src/main/java/com/desafio/globo/infra/persistence/SubscriptionRepository.java
@@ -1,9 +1,12 @@
 package com.desafio.globo.infra.persistence;
 
 import com.desafio.globo.domain.entity.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface SubscriptionRepository extends MongoRepository<Subscription, Long> {}
+public interface SubscriptionRepository extends MongoRepository<Subscription, String> {
+    Optional<Subscription> findBySubscription(String subscriptionId);
+}
