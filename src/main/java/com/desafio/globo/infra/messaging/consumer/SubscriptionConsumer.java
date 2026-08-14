b6ab280a27e3464ff58d4a4d9da7965e73250767
Samuel Lima <samz.xit@gmail.com>
2026-08-14 00:57:22 -0300

    renaming dto reference

--- src/main/java/com/desafio/globo/infra/messaging/consumer/SubscriptionConsumer.java
+++ src/main/java/com/desafio/globo/infra/messaging/consumer/SubscriptionConsumer.java
@@ -1,6 +1,6 @@
 package com.desafio.globo.infra.messaging.consumer;
 
-import com.desafio.globo.domain.dto.RequestSubscriptionDto;
+import com.desafio.globo.api.request.RequestSubscriptionDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
