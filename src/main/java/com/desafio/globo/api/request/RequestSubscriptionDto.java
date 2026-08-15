6f29544adb6433387b49da3de9998c8f7a52c466
Samuel Lima <samz.xit@gmail.com>
2026-08-15 01:08:32 -0300

    Creating a default schema for Swagger

--- src/main/java/com/desafio/globo/api/request/RequestSubscriptionDto.java
+++ src/main/java/com/desafio/globo/api/request/RequestSubscriptionDto.java
@@ -1,8 +1,14 @@
 package com.desafio.globo.api.request;
 
 import com.desafio.globo.domain.enums.NotificationType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RequestSubscriptionDto(
-        String subscription,
-        NotificationType notificationType
+        @Schema(example = "usuario@email.com")
+        @NotBlank String subscription,
+
+        @Schema(example = "SUBSCRIPTION_PURCHASED")
+        @NotNull NotificationType notificationType
 ) {}
