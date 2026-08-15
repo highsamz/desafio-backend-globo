51940ae925abb49ac185cfa945d5c167f28e8962
Samuel Lima <samz.xit@gmail.com>
2026-08-15 01:14:59 -0300

    After consuming the message, calling processing

--- src/main/java/com/desafio/globo/infra/messaging/consumer/SubscriptionConsumer.java
+++ src/main/java/com/desafio/globo/infra/messaging/consumer/SubscriptionConsumer.java
@@ -1,6 +1,7 @@
 package com.desafio.globo.infra.messaging.consumer;
 
 import com.desafio.globo.api.request.RequestSubscriptionDto;
+import com.desafio.globo.application.service.SubscriptionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -9,8 +10,15 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class SubscriptionConsumer {
+
+    SubscriptionService subscriptionService;
+    public SubscriptionConsumer(SubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
     @RabbitListener(queues = "${broker.queue.subscription.name}")
     public void listenEmailQueue(@Payload RequestSubscriptionDto requestSubscriptionDto){
         log.info("Received: {}", requestSubscriptionDto);
+        subscriptionService.process(requestSubscriptionDto);
     }
 }
